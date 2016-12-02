8033943af03e9c9b14f6f9864c2cf305c9f44a82
Yerlan <bayekeshov@gmail.com>
2016-12-02 13:50:56 -0500

    Added infowindow

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -39,6 +39,7 @@ end
 	  	  @hash = Gmaps4rails.build_markers(@places) do |place, marker|
 			  marker.lat place.latitude
 			  marker.lng place.longitude
+        marker.title place.name
 	  	end	  
 	end
 
